b3cc19fd5416dc58be098249996e3ca3121edfd5
Surya Vamsi <suryavamsigara2@gmail.com>
2025-10-23 16:21:41 +0000

    Give LLM access to functions

---
+++ call_function.py
@@ -0,0 +1,27 @@
+from tools.files_info import get_file_info
+from tools.read_file import read_file
+from google.genai import types
+
+working_directory = "testing_directory"
+
+def call_function(function_call):
+
+    print(f"-> Calling function: {function_call.name}({function_call.args})")
+
+    result = ""
+    if function_call.name == "get_file_info":
+        result = get_file_info(working_directory, **function_call.args)
+    elif function_call.name == "read_file":
+        result = read_file(working_directory, **function_call.args)
+    else:
+        result = {"error": f"Unknown function: {function_call.name}"}
+
+    return types.Content(
+        role="function",
+        parts=[
+            types.Part.from_function_response(
+                name=function_call.name,
+                response={"result": result},
+            )
+        ],
+    )
--- main.py
+++ main.py
@@ -2,19 +2,94 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+from tools.files_info import get_file_info_function
+from tools.read_file import read_file_function
+from call_function import call_function
+from typing import Optional, List
+
+WORKING_DIRECTORY = "testing_directory"
 
 def main():
     load_dotenv()
-
-    api_key = os.getenv("GEMINI_API_KEY")
+    api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    system_prompt = """
+    You are an autonomous AI coding agent.
+
+    Your purpose is to understand and operate on the codebase in the working directory to answer questions, analyze functionality, or perform debugging and modification tasks.
+
+    You can perform the following operations (for now):
+    - List files and directories
+    - Read file contents
+
+    Core behavior:
+    1. You must reason about the project structure before taking any actions.
+    2. Always start by calling get_file_info on the working directory to understand what files exist.
+    3. Then, depending on the user request, selectively call read_file on relevant files.
+    4. You should automatically decide which files to inspect or execute — the user does NOT need to specify them.
+    5. Never perform unnecessary or repetitive function calls.
+    7. All paths you reference should be relative to the working directory.
+    8. The working directory is implicitly handled — do not include it in your function calls.
+
+    Your goal is to behave like a self-directed software engineer who can explore, reason about, and act on a local codebase intelligently.
+    """
 
     if len(sys.argv) < 2:
         print("Enter your question..")
         sys.exit(1)
-    
+
     prompt = sys.argv[1]
 
-    print(prompt)
+    tools = types.Tool(
+        function_declarations=[
+            types.FunctionDeclaration(**get_file_info_function),
+            types.FunctionDeclaration(**read_file_function),
+        ]
+    )
 
-main()
+    config = types.GenerateContentConfig(
+        tools=[tools],
+        system_instruction=system_prompt
+    )
+
+    contents = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    max_iters = 20
+    for _ in range(0, max_iters):
+        response = client.models.generate_content(
+            model = "gemini-2.5-flash",
+            contents = contents,
+            config=config,
+        )
+
+        if response is None:
+            print("no response")
+            return
+
+        if response.candidates:
+            contents.append(response.candidates[0].content)
+
+        if response.function_calls:
+            all_parts = []
+            function_calls: Optional[List[types.FunctionCall]] = response.function_calls
+            print(f"\nFUNCTION CALLS: {function_calls}\n")
+            for function_call_part in response.function_calls:
+                result = call_function(function_call_part)
+                print("------------function called-----------")
+                all_parts.extend(result.parts)
+                print(f"-> Function executed: {function_call_part.name}")
+                print("--------------------------------------------------")
+            tool_response = types.Content(role="tool", parts=all_parts)
+            contents.append(tool_response)
+        else:
+            # final agent text message
+            print("-------------------------\nFINAL RESPONSEE\n")
+            print(response.text)
+            return
+
+if __name__ == "__main__":
+    main()
