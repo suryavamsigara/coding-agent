a6d370327745fbd7005b6f7c4fd2411e57c518e9
Surya Vamsi <suryavamsigara2@gmail.com>
2025-10-28 23:08:20 +0000

    Small changes

--- client/quirk/config.py
+++ client/quirk/config.py
@@ -1,10 +1,4 @@
 import os
 
-# root_dir = os.getcwd()
-
 def get_working_directory():
-    client_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    testing_dir = os.path.join(client_dir, "testing_directory")
-    os.makedirs(testing_dir, exist_ok=True)
-    return testing_dir
-    # return os.getcwd()
+    return os.getcwd()
