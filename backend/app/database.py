aeca848ccaa28bc0c17eddef24b12d585747d356
Surya Vamsi <suryavamsigara2@gmail.com>
2025-12-06 18:36:30 +0000

    Small changes

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from .config import settings
+# from .config import settings
 
 load_dotenv()
 
