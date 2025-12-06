5a5541b0c1719df78767418bd5c542d50c82dc7f
Surya Vamsi <suryavamsigara2@gmail.com>
2025-12-06 18:20:00 +0000

    Get db url from .env

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,16 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from .config import settings
 
+load_dotenv()
+
+"""
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+"""
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 if not SQLALCHEMY_DATABASE_URL:
     raise RuntimeError("Database URL is not sent.")
--- backend/app/models.py
+++ backend/app/models.py
@@ -5,15 +5,6 @@ from sqlalchemy.dialects.postgresql import UUID
 from .database import Base
 import uuid
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-
 class ChatSession(Base):
     __tablename__ = "chat_sessions"
 
